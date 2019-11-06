2c046de91c65986b6a21e3f62065f60733e5cc44
midas0615 <bilawals490@gmail.com>
2019-11-07 02:58:49 +0300

    initial commit

---
+++ src/components/App/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const App = () => (
+    <div>
+        <h1>App</h1>
+    </div>
+);
+
+// class App extends Component {
+//     render() {
+//         return(
+//             <div>
+//                 <h1>App</h1>
+//             </div>
+//         );
+//     }
+// }
+
+export default App;
+
